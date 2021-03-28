07e779b7e9984c3e1228394ae76643674cc15bba
EvanEzell <evanezell595@gmail.com>
2021-03-28 14:30:31 -0400

    new exercise

---
+++ MaxProfitWithKTransactions/maxProfitWithKTransactions.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+	if not len(prices):
+	    return 0
+	
+	prevProfits = [0 for d in prices]
+	curProfits = [0 for d in prices]
+
+	for t in range(1,k+1):
+	    maxThusFar = float('-inf')
+
+	    for d in range(1,len(prices)):
+		maxThusFar = max(maxThusFar, prevProfits[d-1] - prices[d-1])
+		curProfits[d] = max(curProfits[d-1], maxThusFar + prices[d])
+		
+	    prevProfits,curProfits = curProfits,prevProfits
+			
+	 return prevProfits[-1]
